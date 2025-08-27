3bdce46e67dad143a65ef7663c267f0d99e023ee
Austin Holbrook <holbrooka96@gmail.com>
2025-08-27 10:55:21 -0400

    Dynamic element filter

--- index.js
+++ index.js
@@ -2,12 +2,8 @@ let teamScore = 0
 let count = 0
 window.addtoScore = function(teamScoreText)
 {
-    if (teamScoreText === 'a') {
-        teamScore = document.getElementById("teamA-score")
-    } else if (teamScoreText = 'b')
-    {
-        teamScore = document.getElementById("teamB-score")
-    }
+    teamScore = "team"+teamScoreText.toUpperCase()+"-score"
+    teamScore = document.getElementById(teamScore)
     count = Number(teamScore.textContent)
     count += Number(Object.values(event.target.innerText).join(''))
     teamScore.textContent = count
