33418e570b324d39d942ff503b2ad9b6f01fa520
Austin Holbrook <holbrooka96@gmail.com>
2025-08-27 08:20:08 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,14 @@
+let teamScore = 0
+let count = 0
+function addtoScore(teamScoreText)
+{
+    if (teamScoreText === 'a') {
+        teamScore = document.getElementById("teamA-score")
+    } else if (teamScoreText = 'b')
+    {
+        teamScore = document.getElementById("teamB-score")
+    }
+    count = Number(teamScore.textContent)
+    count += Number(Object.values(event.target.innerText).join(''))
+    teamScore.textContent = count
+}
